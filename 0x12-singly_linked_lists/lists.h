50ce89b056549ba5a9ca528595148e673018bb4d
huda salah <huda.1425997@gmail.com>
2023-10-19 05:01:11 -0700

    finished

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-/*
-* Desc: header file containinf prototypes and definitions
-*	for all functions and types written in this proj
-*/
-
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,6 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
